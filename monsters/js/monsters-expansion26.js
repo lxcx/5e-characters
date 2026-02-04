570263e9734614cb4ad32fa7080c91d91df66c08
McFly <chris@nerd.com>
2026-02-04 18:55:29 -0500

    Consolidate 92 monster data files into single file for faster loading

--- monsters/js/monsters-expansion26.js
+++
@@ -1,6 +0,0 @@
-// Expansion Pack 26 - More Creatures
-// Entries moved to other files to eliminate duplicates
-
-const MONSTERS_EXPANSION26 = {
-    // Oblex entries are in monsters-eladrin.js as oblex-spawn, oblex-adult, oblex-elder
-};
